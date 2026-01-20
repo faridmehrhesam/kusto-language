2e3e80485ec3bc02d989f4712f54218126c163ad
Hesam Faridmehr <faridmehr.hesam@gmail.com>
2026-01-20 20:43:43 +0100

    Refactored and reorganized (#11)

---
+++ src/token_parser/mod.rs
@@ -0,0 +1,159 @@
+use std::ops::Range;
+
+mod parser;
+mod scanner;
+mod utilities;
+
+#[cfg(test)]
+mod tests;
+
+pub use parser::parse_tokens;
+
+const MULTI_LINE_STRING_SEQUENCES: &[&[u8]] = &[b"```", b"~~~"];
+const AVG_BYTES_PER_TOKEN: usize = 5;
+const TIMESPAN_SUFFIXES: &[&[u8]] = &[
+    b"microseconds",
+    b"milliseconds",
+    b"nanoseconds",
+    b"microsecond",
+    b"millisecond",
+    b"nanosecond",
+    b"microsec",
+    b"millisec",
+    b"nanosec",
+    b"minutes",
+    b"seconds",
+    b"hours",
+    b"micros",
+    b"millis",
+    b"nanos",
+    b"minute",
+    b"second",
+    b"hour",
+    b"micro",
+    b"milli",
+    b"nano",
+    b"ticks",
+    b"tick",
+    b"hrs",
+    b"sec",
+    b"min",
+    b"day",
+    b"ms",
+    b"hr",
+    b"d",
+    b"h",
+    b"m",
+    b"s",
+];
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq)]
+pub enum SyntaxKind {
+    // punctuation tokens
+    OpenParenToken,
+    CloseParenToken,
+    OpenBracketToken,
+    CloseBracketToken,
+    OpenBraceToken,
+    CloseBraceToken,
+    BarToken,
+    DotToken,
+    DotDotToken,
+    PlusToken,
+    MinusToken,
+    AsteriskToken,
+    SlashToken,
+    PercentToken,
+    LessThanToken,
+    LessThanOrEqualToken,
+    LessThanBarToken,
+    LessThanGreaterThanToken,
+    GreaterThanToken,
+    GreaterThanOrEqualToken,
+    EqualToken,
+    EqualEqualToken,
+    FatArrowToken,
+    EqualTildeToken,
+    BangEqualToken,
+    BangTildeToken,
+    ColonToken,
+    SemicolonToken,
+    CommaToken,
+    AtToken,
+    QuestionToken,
+
+    // literal tokens
+    LongLiteralToken,
+    RealLiteralToken,
+    TimespanLiteralToken,
+    RawGuidLiteralToken,
+    StringLiteralToken,
+
+    // identifier
+    IdentifierToken,
+
+    // other tokens
+    DirectiveToken,
+    EndOfTextToken,
+    BadToken,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ParseOptions {
+    pub always_produce_end_tokens: bool,
+}
+
+impl Default for ParseOptions {
+    fn default() -> Self {
+        Self {
+            always_produce_end_tokens: true,
+        }
+    }
+}
+
+impl ParseOptions {
+    pub fn new(always_produce_end_tokens: bool) -> Self {
+        Self {
+            always_produce_end_tokens,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct LexicalToken {
+    kind: SyntaxKind,
+    trivia_span: Range<usize>,
+    text_span: Range<usize>,
+}
+
+impl LexicalToken {
+    pub fn new(kind: SyntaxKind, trivia: Range<usize>, text: Range<usize>) -> Self {
+        Self {
+            kind,
+            trivia_span: trivia,
+            text_span: text,
+        }
+    }
+
+    pub fn kind(&self) -> SyntaxKind {
+        self.kind
+    }
+
+    pub fn trivia_span(&self) -> &Range<usize> {
+        &self.trivia_span
+    }
+
+    pub fn text_span(&self) -> &Range<usize> {
+        &self.text_span
+    }
+
+    /// Returns total length (trivia + text)
+    pub fn len(&self) -> usize {
+        (self.trivia_span.end - self.trivia_span.start)
+            + (self.text_span.end - self.text_span.start)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
---
+++ src/token_parser/utilities.rs
@@ -0,0 +1,96 @@
+#[inline(always)]
+pub(crate) fn peek(bytes: &[u8], pos: usize) -> Option<&u8> {
+    bytes.get(pos)
+}
+
+#[inline(always)]
+pub(crate) fn is_at_end(bytes: &[u8], pos: usize) -> bool {
+    pos >= bytes.len()
+}
+
+#[inline(always)]
+pub(crate) fn is_line_break_start(byte: u8) -> bool {
+    byte == b'\r' || byte == b'\n'
+}
+
+#[inline(always)]
+pub(crate) fn is_string_literal_start_quote(byte: u8) -> bool {
+    byte == b'"' || byte == b'\'' || byte == b'`' || byte == b'~'
+}
+
+#[inline(always)]
+pub(crate) fn is_identifier_start_char(byte: u8) -> bool {
+    byte.is_ascii_alphabetic() || byte == b'_' || byte == b'$'
+}
+
+#[inline(always)]
+pub(crate) fn is_identifier_char(byte: u8) -> bool {
+    byte.is_ascii_alphanumeric() || byte == b'_'
+}
+
+pub(crate) fn count_while<F>(bytes: &[u8], start: usize, predicate: F) -> usize
+where
+    F: Fn(&u8) -> bool,
+{
+    let mut pos = start;
+
+    while let Some(byte) = peek(bytes, pos)
+        && predicate(byte)
+    {
+        pos += 1;
+    }
+
+    pos - start
+}
+
+pub(crate) fn matches_sequence(bytes: &[u8], start: usize, sequence: &[u8]) -> bool {
+    bytes.get(start..start + sequence.len()) == Some(sequence)
+}
+
+pub(crate) fn get_next_line_start(bytes: &[u8], start: usize) -> Option<usize> {
+    let next_start = get_next_line_break_start(bytes, start)?;
+    let line_break_len = get_next_line_break_len(bytes, next_start)?;
+
+    Some(next_start + line_break_len)
+}
+
+pub(crate) fn get_line_end(bytes: &[u8], start: usize) -> usize {
+    start + get_line_len(bytes, start, false)
+}
+
+fn get_line_len(bytes: &[u8], start: usize, include_line_break: bool) -> usize {
+    let mut pos = start;
+
+    while let Some(&byte) = peek(bytes, pos)
+        && !is_line_break_start(byte)
+    {
+        pos += 1;
+    }
+
+    if include_line_break && let Some(line_break_len) = get_next_line_break_len(bytes, start) {
+        pos += line_break_len
+    }
+
+    pos - start
+}
+
+fn get_next_line_break_len(bytes: &[u8], start: usize) -> Option<usize> {
+    let break_start = get_next_line_break_start(bytes, start)?;
+
+    if peek(bytes, break_start) == Some(&b'\r') && peek(bytes, break_start + 1) == Some(&b'\n') {
+        Some(2)
+    } else {
+        Some(1)
+    }
+}
+
+fn get_next_line_break_start(bytes: &[u8], start: usize) -> Option<usize> {
+    if let Some(rel_pos) = bytes[start..]
+        .iter()
+        .position(|&b| b == b'\n' || b == b'\r')
+    {
+        return Some(rel_pos + start);
+    }
+
+    None
+}
